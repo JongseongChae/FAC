cb33430a574be9dd14d5352b14c42cbe7b548015
Jongseong Chae <106060295+JongseongChae@users.noreply.github.com>
2026-02-10 01:53:02 +0900

    Add files via upload

---
+++ utils/datasets.py
@@ -0,0 +1,216 @@
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+from flax.core.frozen_dict import FrozenDict
+
+import tqdm
+
+
+def get_size(data):
+    """Return the size of the dataset."""
+    sizes = jax.tree_util.tree_map(lambda arr: len(arr), data)
+    return max(jax.tree_util.tree_leaves(sizes))
+
+
+@partial(jax.jit, static_argnames=('padding',))
+def random_crop(img, crop_from, padding):
+    """Randomly crop an image.
+
+    Args:
+        img: Image to crop.
+        crop_from: Coordinates to crop from.
+        padding: Padding size.
+    """
+    padded_img = jnp.pad(img, ((padding, padding), (padding, padding), (0, 0)), mode='edge')
+    return jax.lax.dynamic_slice(padded_img, crop_from, img.shape)
+
+
+@partial(jax.jit, static_argnames=('padding',))
+def batched_random_crop(imgs, crop_froms, padding):
+    """Batched version of random_crop."""
+    return jax.vmap(random_crop, (0, 0, None))(imgs, crop_froms, padding)
+
+
+class Dataset(FrozenDict):
+    """Dataset class."""
+
+    @classmethod
+    def create(cls, freeze=True, **fields):
+        """Create a dataset from the fields.
+
+        Args:
+            freeze: Whether to freeze the arrays.
+            **fields: Keys and values of the dataset.
+        """
+        data = fields
+        assert 'observations' in data
+        if freeze:
+            jax.tree_util.tree_map(lambda arr: arr.setflags(write=False), data)
+        return cls(data)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.size = get_size(self._dict)
+        self.frame_stack = None # Number of frames to stack
+        self.p_aug = None # Image augmentation probability
+        self.return_next_actions = False # Whether to additionally return next actions
+
+        # Compute terminal and initial locations.
+        self.terminal_locs = np.nonzero(self['terminals'] > 0)[0]
+        self.initial_locs = np.concatenate([[0], self.terminal_locs[:-1] + 1])
+
+        # Compute minimum log-density
+        self.estimated_logp_min = None
+
+    def get_random_idxs(self, num_idxs):
+        """Return `num_idxs` random indices."""
+        return np.random.randint(self.size, size=num_idxs)
+
+    def sample(self, batch_size: int, idxs=None):
+        """Sample a batch of transitions."""
+        if idxs is None:
+            idxs = self.get_random_idxs(batch_size)
+        batch = self.get_subset(idxs)
+        if self.frame_stack is not None:
+            # Stack frames.
+            initial_state_idxs = self.initial_locs[np.searchsorted(self.initial_locs, idxs, side='right') - 1]
+            obs = [] # Will be [ob[t - frame_stack + 1], ..., ob[t]].
+            next_obs = [] # Will be [ob[t - frame_stack + 2], ..., ob[t], next_ob[t]].
+            for i in reversed(range(self.frame_stack)):
+                # Use the initial state if the index is out of bounds.
+                cur_idxs = np.maximum(idxs - i, initial_state_idxs)
+                obs.append(jax.tree_util.tree_map(lambda arr: arr[cur_idxs], self['observations']))
+                if i != self.frame_stack - 1:
+                    next_obs.append(jax.tree_util.tree_map(lambda arr: arr[cur_idxs], self['observations']))
+            next_obs.append(jax.tree_util.tree_map(lambda arr: arr[idxs], self['next_observations']))
+
+            batch['observations'] = jax.tree_util.tree_map(lambda *args: np.concatenate(args, axis=-1), *obs)
+            batch['next_observations'] = jax.tree_util.tree_map(lambda *args: np.concatenate(args, axis=-1), *next_obs)
+        if self.p_aug is not None:
+            # Apply random-crop image augmentation.
+            if np.random.rand() < self.p_aug:
+                self.augment(batch, ['observations', 'next_observations'])
+                
+        return batch
+
+    def get_subset(self, idxs):
+        """Return a subset of the dataset given the indices."""
+        result = jax.tree_util.tree_map(lambda arr: arr[idxs], self._dict)
+        if self.return_next_actions:
+            # WARNING: This is incorrect at the end of the trajectory. Use with caution.
+            result['next_actions'] = self._dict['actions'][np.minimum(idxs + 1, self.size - 1)]
+        return result
+
+    def augment(self, batch, keys):
+        """Apply image augmentation to the given keys."""
+        padding = 3
+        batch_size = len(batch[keys[0]])
+        crop_froms = np.random.randint(0, 2 * padding + 1, (batch_size, 2))
+        crop_froms = np.concatenate([crop_froms, np.zeros((batch_size, 1), dtype=np.int64)], axis=1)
+        for key in keys:
+            batch[key] = jax.tree_util.tree_map(
+                lambda arr: np.array(batched_random_crop(arr, crop_froms, padding)) if len(arr.shape) == 4 else arr,
+                batch[key],
+            )
+    
+    def compute_and_attach_estimated_logp(self, agent, method, batch_size=8192):
+        """Compute log beta(a|s) for the entire dataset and attach it to self._dict['estimated_logp'] as float32."""
+        length = getattr(self, 'max_size', self.size) # max_size if ReplayBuffer else dataset.size
+        if 'estimated_logp' not in self._dict:
+            self._dict['estimated_logp'] = np.full((length,), np.nan, dtype=np.float32)
+
+        old_p_aug = self.p_aug
+        self.p_aug = None
+
+        all_indices = np.arange(self.size, dtype=np.int64)
+        num_chunks = (self.size + batch_size - 1) // batch_size
+
+        local_rng = agent.rng
+        for ci in tqdm.tqdm(range(num_chunks), smoothing=0.1, dynamic_ncols=True, desc='compute logp'):
+            start = ci * batch_size
+            end = min((ci + 1) * batch_size, self.size)
+            idxs = all_indices[start:end]
+
+            batch = self.sample(batch_size=(end - start), idxs=idxs)
+
+            local_rng, key = jax.random.split(local_rng)
+            logp = agent.logprob_given_actions(
+                observations=batch['observations'],
+                actions_final=batch['actions'],
+                rng=key,
+                mode=method,
+            )
+
+            self._dict['estimated_logp'][idxs] = np.array(logp, dtype=np.float32)
+
+        self.p_aug = old_p_aug
+
+        # Compute the minimum of estimated log-densities over dataset.
+        logp_min = np.nanmin(self._dict['estimated_logp'][: self.size]).astype(np.float32)
+        self._dict['estimated_logp_min'] = np.full((length,), logp_min, dtype=np.float32)
+        self.estimated_logp_min = float(logp_min)
+
+
+class ReplayBuffer(Dataset):
+    """Replay buffer class.
+
+    This class extends Dataset to support adding transitions.
+    """
+
+    @classmethod
+    def create(cls, transition, size):
+        """Create a replay buffer from the example transition.
+
+        Args:
+            transition: Example transition (dict).
+            size: Size of the replay buffer.
+        """
+
+        def create_buffer(example):
+            example = np.array(example)
+            return np.zeros((size, *example.shape), dtype=example.dtype)
+
+        buffer_dict = jax.tree_util.tree_map(create_buffer, transition)
+        return cls(buffer_dict)
+
+    @classmethod
+    def create_from_initial_dataset(cls, init_dataset, size):
+        """Create a replay buffer from the initial dataset.
+
+        Args:
+            init_dataset: Initial dataset.
+            size: Size of the replay buffer.
+        """
+
+        def create_buffer(init_buffer):
+            buffer = np.zeros((size, *init_buffer.shape[1:]), dtype=init_buffer.dtype)
+            buffer[: len(init_buffer)] = init_buffer
+            return buffer
+
+        buffer_dict = jax.tree_util.tree_map(create_buffer, init_dataset)
+        dataset = cls(buffer_dict)
+        dataset.size = dataset.pointer = get_size(init_dataset)
+        return dataset
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.max_size = get_size(self._dict)
+        self.size = 0
+        self.pointer = 0
+
+    def add_transition(self, transition):
+        """Add a transition to the replay buffer."""
+
+        def set_idx(buffer, new_element):
+            buffer[self.pointer] = new_element
+
+        jax.tree_util.tree_map(set_idx, self._dict, transition)
+        self.pointer = (self.pointer + 1) % self.max_size
+        self.size = max(self.pointer, self.size)
+
+    def clear(self):
+        """Clear the replay buffer."""
+        self.size = self.pointer = 0
